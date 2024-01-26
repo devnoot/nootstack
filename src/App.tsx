9e7cbb7a7c455134432513df5052ec0dbc31c47b
Anthony W. Weed <devnoot@gmail.com>
2024-01-25 22:44:23 -0500

    Added tailwind-merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Provider } from 'react-redux'
 import { store } from './features/store/store.tsx'
-import { Layout } from './Layout.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './features/router/router.tsx'
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,22 @@
+import { twMerge } from 'tailwind-merge'
+
 export const Home = () => {
-  return <><h1 className='text-3xl'>Hello</h1></>
+  return (
+    <div className={twMerge(
+      'h-screen',
+      'w-screen',
+      'max-w-screen',
+      'min-h-screen',
+      'flex',
+      'flex-col',
+      'bg-gray-50',
+      'dark:bg-slate-800',
+      'text-black',
+      'dark:text-white',
+      'justify-center',
+      'items-center'
+    )}>
+      <h1 className={twMerge('text-4xl')}>Hello</h1>
+    </div>
+  )
 }
